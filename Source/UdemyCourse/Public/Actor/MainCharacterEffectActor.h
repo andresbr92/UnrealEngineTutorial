5e774309065c3a59205e4c82acb88498876653f8
andresbr92 <andresbr92@gmail.com>
2024-11-26 16:32:09 +0100

    Add new effect classes to MainCharacterEffectActor

--- Source/UdemyCourse/Public/Actor/MainCharacterEffectActor.h
+++ Source/UdemyCourse/Public/Actor/MainCharacterEffectActor.h
@@ -28,6 +28,12 @@ protected:
 	
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category= "Applied Effects")
 	TSubclassOf<UGameplayEffect> InstantGameplayEffectClass;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category= "Applied Effects")
+	TSubclassOf<UGameplayEffect> DurationGameplayEffectClass;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Applied Effects")
+	TSubclassOf<UGameplayEffect> InfiniteGameplayEffectClass;
 	
 
 };
